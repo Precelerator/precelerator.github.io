64749035f9b1d70bee678b7b313acec3a5918afb
Precelerator (SCE) <70708347+Precelerator@users.noreply.github.com>
2020-10-28 17:34:37 +0100

    Remove Beratungssession form

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Layout from '../components/layout'
 import Header from '../sections/header'
 import Institutions from '../sections/institutions'
 import Services from '../sections/services'
-import RequestForm from '../sections/request-form'
 import About from '../sections/about'
 import Contact from '../sections/contact'
 
@@ -12,7 +11,6 @@ export default function Home() {
       <Header></Header>
       <Institutions></Institutions>
       <Services></Services>
-      <RequestForm></RequestForm>
       <About></About>
       <Contact></Contact>
     </Layout>
