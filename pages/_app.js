d9f2494cad5330c1f6dc51e332030c3c2f0776fc
Precelerator (SCE) <70708347+Precelerator@users.noreply.github.com>
2020-10-30 12:21:05 +0100

    Update services section, change logo order

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import '../assets/css/bootstrap.css'
-import '../assets/css/fontawesome-all.css'
-import '../assets/css/swiper.css'
-import '../assets/css/magnific-popup.css'
-import '../assets/css/styles.css'
+import '../assets/css/bootstrap.css';
+import '../assets/css/fontawesome-all.css';
+import '../assets/css/swiper.css';
+import '../assets/css/magnific-popup.css';
+import '../assets/css/styles.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- sections/services.js
+++ sections/services.js
@@ -1,5 +1,9 @@
 import ListEntry from '../components/list-entry';
 import Card from '../components/card';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 export default function Services() {
   return (
@@ -20,60 +24,117 @@ export default function Services() {
           </div>
           <div className="row">
             <div className="col-lg-12 flex-container">
-              <Card img="/images/services-icon-1.svg" title="CONNECT.">
-                <p>
-                  Der Precelerator bringt Ideensucher und Interessierte mit
-                  Fachexperten und Ideengebern zusammen. Damit ist er der ideale
-                  Co-Working Space, an dem alle Fakultäten zusammenkommen und
-                  sich austauschen können.
-                </p>
-                <ul className="list-unstyled li-space-lg">
-                  <ListEntry
-                    strongText="Kontakte und Netzwerk"
-                    regularText="über das SCE und die HM"
-                  />
-                  <ListEntry
-                    strongText="Vernetzung und Inspiration"
-                    regularText="durch die Precelerator Projektwand"
-                  />
-                </ul>
-              </Card>
-
-              <Card img="/images/services-icon-2.svg" title="TRAIN.">
-                <p>
-                  Der Precelerator stellt Ressourcen, Ansprechpartner und
-                  Anregungen bereit, mithilfe derer du die für dein Projekt
-                  notwendigen Skills aneignen kannst.
-                </p>
-                <ul className="list-unstyled li-space-lg">
-                  <ListEntry
-                    strongText="Ressourcen und Anregungen"
-                    regularText="rund um Entrepreneurship und Entrepreneurial Thinking"
-                  />
-                  <ListEntry
-                    strongText="Technische Beratung"
-                    regularText="zur Umsetzung von Prototypen (egal ob digital oder analog)"
-                  />
-                </ul>
-              </Card>
-
-              <Card img="/images/services-icon-3.svg" title="CREATE.">
-                <p>
-                  Der Precelerator versteht sich als Ort, an dem Neues
-                  geschaffen wird - seien es innovative Geschäftsmodelle,
-                  digitale Technologien oder gefertigte Prototypen.
-                </p>
-                <ul className="list-unstyled li-space-lg">
-                  <ListEntry
-                    strongText="Gadgets & Werkzeuge"
-                    regularText="zum Ausprobieren, Lehrveranstaltungen und Demos"
-                  />
-                  <ListEntry
-                    strongText="Modernste Ausstattung:"
-                    regularText="3D-Drucker, High-End Rechner, Filmstudio, VR-Brillen, etc."
-                  />
-                </ul>
-              </Card>
+              <Swiper
+                navigation
+                pagination
+                autoplay={{
+                  delay: 10000,
+                }}
+                breakpoints={{
+                  320: {
+                    slidesPerView: 1,
+                  },
+                  1200: {
+                    slidesPerView: 3,
+                  },
+                }}
+              >
+                <SwiperSlide>
+                  <Card img="/images/services-icon-1.svg" title="CONNECT.">
+                    <p>
+                      Der Precelerator bringt Ideensucher und Interessierte mit
+                      Fachexperten und Ideengebern zusammen. Damit ist er der
+                      ideale Co-Working Space, an dem alle Fakultäten
+                      zusammenkommen und sich austauschen können.
+                    </p>
+                    <ul className="list-unstyled li-space-lg">
+                      <ListEntry
+                        strongText="Kontakte und Netzwerk"
+                        regularText="über das SCE und die HM"
+                      />
+                      <ListEntry
+                        strongText="Vernetzung und Inspiration"
+                        regularText="durch die Precelerator Projektwand"
+                      />
+                    </ul>
+                  </Card>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Card img="/images/services-icon-2.svg" title="TRAIN.">
+                    <p>
+                      Der Precelerator stellt Ressourcen, Ansprechpartner und
+                      Anregungen bereit, mithilfe derer du die für dein Projekt
+                      notwendigen Skills aneignen kannst.
+                    </p>
+                    <ul className="list-unstyled li-space-lg">
+                      <ListEntry
+                        strongText="Ressourcen und Anregungen"
+                        regularText="rund um Entrepreneurship und Entrepreneurial Thinking"
+                      />
+                      <ListEntry
+                        strongText="Technische Beratung"
+                        regularText="zur Umsetzung von Prototypen (egal ob digital oder analog)"
+                      />
+                    </ul>
+                  </Card>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Card img="/images/services-icon-3.svg" title="CREATE.">
+                    <p>
+                      Im Precelerator kannst du deine erworbenen Mtehoden und
+                      Kompetenzen auf eigene Ideen, unternehmerische Vorhaben
+                      und Geschäftsmodelle übertragen.
+                    </p>
+                    <ul className="list-unstyled li-space-lg">
+                      <ListEntry
+                        strongText="Co-Working Spaces"
+                        regularText="zum Arbeiten an deiner Idee"
+                      />
+                      <ListEntry
+                        strongText="Workshops & Formate"
+                        regularText="um deine Ideen gezielt weiterzuentwickeln"
+                      />
+                    </ul>
+                  </Card>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Card img="/images/services-icon-3.svg" title="MAKE.">
+                    <p>
+                      Der Precelerator versteht sich als Ort, an dem Neues
+                      geschaffen wird - seien es innovative Geschäftsmodelle,
+                      digitale Technologien oder gefertigte Prototypen.
+                    </p>
+                    <ul className="list-unstyled li-space-lg">
+                      <ListEntry
+                        strongText="Werkzeuge & Werkstatt-Maschinen"
+                        regularText="zum Ausprobieren, Lehrveranstaltungen und Demos"
+                      />
+                      <ListEntry
+                        strongText="Modernste Ausstattung:"
+                        regularText="3D-Drucker, High-End Rechner, Filmstudio, VR-Brillen, etc."
+                      />
+                    </ul>
+                  </Card>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Card img="/images/services-icon-3.svg" title="PLAY.">
+                    <p>
+                      Im Precelerator erlernst du spielerisch den Umgang mit
+                      moderner Technik und Ansätzen.
+                    </p>
+                    <ul className="list-unstyled li-space-lg">
+                      <ListEntry
+                        strongText="Gadgets"
+                        regularText="zum Ausprobieren, Lehrveranstaltungen und Demos"
+                      />
+                      <ListEntry
+                        strongText="Spielerische Formate"
+                        regularText="Wettbewerbe, Hackathons und Games rund um Entrepreneurship & Prototyping"
+                      />
+                    </ul>
+                  </Card>
+                </SwiperSlide>
+              </Swiper>
             </div>
             {/* end of col */}
           </div>
