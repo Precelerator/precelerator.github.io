ef58e5957b4131d31ef72f316be0a156cf2aeff0
Precelerator <70708347+Precelerator@users.noreply.github.com>
2020-11-04 14:50:30 +0000

    Deploying to gh-pages from  @ 3a16950ef82bc183521350f7b40a9d3a6ccf55f6 🚀

--- js/klaro/google-analytics.js
+++ js/klaro/google-analytics.js
@@ -1,20 +1,37 @@
-
-var gaProperty = 'G-ZEJ45LTFJV';
+var gaProperty = 'UA-181872956-1';
 var disableStr = 'ga-disable-' + gaProperty;
 if (document.cookie.indexOf(disableStr + '=true') > -1) {
-window[disableStr] = true;
+  window[disableStr] = true;
 }
 function gaOptout() {
-document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-window[disableStr] = true;
-alert('Das Tracking durch Google Analytics wurde in Ihrem Browser für diese Website deaktiviert.');
+  document.cookie =
+    disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+  window[disableStr] = true;
+  alert(
+    'Das Tracking durch Google Analytics wurde in Ihrem Browser für diese Website deaktiviert.',
+  );
 }
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'G-ZEJ45LTFJV', 'auto');
+(function (i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  (i[r] =
+    i[r] ||
+    function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(
+  window,
+  document,
+  'script',
+  'https://www.google-analytics.com/analytics.js',
+  'ga',
+);
+ga('create', 'UA-181872956-1', 'auto');
 ga('require', 'displayfeatures');
 ga('require', 'linkid', 'linkid.js');
 ga('set', 'anonymizeIp', true);
-ga('send', 'pageview')
+ga('send', 'pageview');
