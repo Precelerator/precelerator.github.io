5d5033ead9d913fe4a11446ca152c33949b44589
Precelerator (SCE) <70708347+Precelerator@users.noreply.github.com>
2020-12-08 14:00:46 +0100

    Add newsletter page, iframe as temporary workaround

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -1,15 +1,14 @@
 import Layout from '../components/layout';
 import SiteHeader from '../components/site-header';
 import TextContainer from '../components/text-container';
-import NewsletterForm from '../components/newsletter-form';
 import Breadcrumbs from '../components/breadcrumbs';
 
 export default function Impressum() {
   return (
     <Layout home={false}>
-      <SiteHeader heading="Newsletter"></SiteHeader>
+      {/* <SiteHeader heading="Newsletter"></SiteHeader> */}
       <TextContainer>
-        <h3>Anmeldung zum Newsletter</h3>
+        <h3 class="newsletter">Anmeldung zum Newsletter</h3>
         <p>
           Der Precelerator-Newsletter versorgt dich mit aktuellen Informationen
           rund um anstehende Angebote und Veranstaltungen. Wähle einfach unten
@@ -20,7 +19,13 @@ export default function Impressum() {
           Mit dem Newsletter bleibst du auf dem Laufenden und verpasst keine
           Precelerator-Updates - kein Spam, versprochen!
         </p>
-        <NewsletterForm></NewsletterForm>
+        <iframe
+          id="newsletterFrame"
+          width="100%"
+          src="https://precelerator.de/newsletter-iframe/"
+        ></iframe>
+
+        {/* <NewsletterForm></NewsletterForm> */}
       </TextContainer>
       <Breadcrumbs siteName="Impressum"></Breadcrumbs>
     </Layout>
