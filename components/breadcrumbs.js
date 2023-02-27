73d1b1dad771a72be80426d957e8514d429d9f02
Precelerator (SCE) <70708347+Precelerator@users.noreply.github.com>
2023-02-27 15:11:41 +0100

    Update breadcrumbs.js
    redirect "\" to https://www.sce.de/precelerator/

--- components/breadcrumbs.js
+++ components/breadcrumbs.js
@@ -5,7 +5,7 @@ export default function Breadcrumbs({ siteName} ) {
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="breadcrumbs">
-                        <a href="/">Home</a><i className="fa fa-angle-double-right"></i><span>{siteName}</span>
+                        <a href="https://www.sce.de/precelerator/">Home</a><i className="fa fa-angle-double-right"></i><span>{siteName}</span>
                         </div>
                     </div>
                 </div>
